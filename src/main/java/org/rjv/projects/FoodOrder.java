b5085e25d8646fb77f5a7b50e29b35e7330ac458
Rajeev Haridas <rajeev.h@tataelxsi.co.in>
2023-04-05 16:55:30 +0400

    Initial commit

---
+++ src/main/java/org/rjv/projects/FoodOrder.java
@@ -0,0 +1,21 @@
+package org.rjv.projects;
+
+public class FoodOrder {
+    public static void main(String[] args) {
+        System.out.println("Protein Level : "+ getProteinLevel(new Dish<>( new Cheese(), new Egg())));
+    }
+
+    private static int getProteinLevel(Dish<HighProtein> highProteinDish) {
+        return switch (highProteinDish){
+            case Dish<HighProtein>(Egg egg, Cheese cheese)  -> 1;
+            case Dish<HighProtein>(Cheese cheese, Egg egg)  -> 2;
+            default -> throw new IllegalStateException("Unexpected value: " + highProteinDish);
+        };
+    }
+}
+
+sealed interface HighProtein permits Egg, Cheese {}
+final class Egg implements HighProtein {}
+final class Cheese implements HighProtein {}
+
+record Dish<T> (T x, T y){}
